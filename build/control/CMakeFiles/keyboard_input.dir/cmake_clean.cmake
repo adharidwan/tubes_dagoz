d5b005a30be089c3533e78b0c25bfb6abf574be9
adharidwan <adharidwan2115@gmail.com>
2024-09-24 23:47:26 +0700

    keyboard_input

---
+++ build/control/CMakeFiles/keyboard_input.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/keyboard_input.dir/src/keyboard_input.cpp.o"
+  "CMakeFiles/keyboard_input.dir/src/keyboard_input.cpp.o.d"
+  "keyboard_input"
+  "keyboard_input.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/keyboard_input.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/control/src/keyboard_input.cpp
@@ -0,0 +1,103 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <ncurses.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+class KeyboardInputNode : public rclcpp::Node
+{
+public:
+    KeyboardInputNode() : Node("keyboard_input_node"), velocity_duration_(1.0)
+    {
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+
+        initscr();
+        cbreak();
+        noecho(); 
+        keypad(stdscr, TRUE); 
+
+        velocity_msg_.linear.x = 0.0;
+        velocity_msg_.angular.z = 0.0;
+
+        control_thread_ = std::thread(&KeyboardInputNode::controlLoop, this);
+    }
+
+    ~KeyboardInputNode()
+    {
+        endwin();
+        if (control_thread_.joinable()) {
+            control_thread_.join();
+        }
+    }
+
+private:
+    void controlLoop()
+    {
+        int ch;
+        while (rclcpp::ok())
+        {
+            ch = getch(); 
+            // auto start_time = std::chrono::steady_clock::now(); 
+            switch (ch)
+            {
+                case 'w':
+                    updateVelocity(1.0, 0.0);
+                    break;
+                case 's':
+                    updateVelocity(-1.0, 0.0);
+                    break;
+                case 'a':
+                    updateVelocity(0.0, 1.0);
+                    break;
+                case 'd':
+                    updateVelocity(0.0, -1.0);
+                    break;
+                case 'q':
+                    RCLCPP_INFO(this->get_logger(), "Exiting...");
+                    rclcpp::shutdown();
+                    return;
+                default:
+                    break;
+            }
+
+            std::this_thread::sleep_for(std::chrono::duration<double>(velocity_duration_));
+            resetVelocity();
+
+        }
+    }
+
+    void updateVelocity(double linear, double angular)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        velocity_msg_.linear.x = linear;
+        velocity_msg_.angular.z = angular;
+        publisher_->publish(velocity_msg_);
+        RCLCPP_INFO(this->get_logger(), "Published linear velocity: %.2f, angular velocity: %.2f",
+                    velocity_msg_.linear.x, velocity_msg_.angular.z);
+    }
+
+    void resetVelocity()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        velocity_msg_.linear.x = 0.0;
+        velocity_msg_.angular.z = 0.0;
+        publisher_->publish(velocity_msg_);
+        RCLCPP_INFO(this->get_logger(), "Resetted back after 1s = linear velocity : %.2f, angular velocity: %.2f",velocity_msg_.linear.x, velocity_msg_.angular.z);
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    geometry_msgs::msg::Twist velocity_msg_;
+    std::thread control_thread_;
+    double velocity_duration_;  
+    std::mutex mutex_;           
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<KeyboardInputNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
