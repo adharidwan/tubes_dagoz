be6d29931bf11cc9c5e51175452f85f9c3a0e447
adharidwan <adharidwan2115@gmail.com>
2024-11-16 15:24:50 +0700

    feat:added terminator

--- src/control/launch/control.launch.py
+++ src/control/launch/control.launch.py
@@ -1,21 +1,53 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
 
 def generate_launch_description():
+    # Create a temporary layout config file
+    config_content = """
+[layouts]
+  [[default]]
+    [[[window0]]]
+      type = Window
+      parent = ""
+      size = 1000, 600
+    [[[child1]]]
+      type = VPaned
+      parent = window0
+      position = 300
+    [[[child2]]]
+      type = HPaned
+      parent = child1
+      position = 500
+    [[[terminal1]]]
+      type = Terminal
+      parent = child2
+      profile = default
+      command = "bash -c 'echo \"Keyboard Input Node\" && ros2 run control keyboard_input; exec bash'"
+    [[[terminal2]]]
+      type = Terminal
+      parent = child2
+      profile = default
+      command = "bash -c 'echo \"Translator Locomotion Node\" && ros2 run control translator_locomotion; exec bash'"
+    [[[terminal3]]]
+      type = Terminal
+      parent = child1
+      profile = default
+      command = "bash -c 'echo \"Goalkeeper Arm Node\" && ros2 run control translator_expanding; exec bash'"
+"""
+    
+    # Write the config to a temporary file
+    config_path = "/tmp/terminator_layout.conf"
+    with open(config_path, "w") as f:
+        f.write(config_content)
+    
+    # Command to launch terminator with the config
+    terminator_cmd = f"terminator -l default -g {config_path}"
+
     return LaunchDescription([
-        Node(
-            package='control',
-            executable='keyboard_input',
-            name='keyboard_input_node'
-        ),
-        Node(
-            package='control',
-            executable='translator_locomotion',
-            name='translator_node'
-        ),
-        Node(
-            package='control',
-            executable='translator_expanding',
-            name='goalkeeper_arm_mover'
+        ExecuteProcess(
+            cmd=['bash', '-c', terminator_cmd],
+            name='terminator_windows'
         )
     ])
